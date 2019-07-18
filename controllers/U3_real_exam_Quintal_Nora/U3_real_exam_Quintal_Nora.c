1778f32e368bc2f9d9aa7b30df5ae141b102971a
N-halila <al1609106@upy.edu.mx>
2019-07-18 05:23:16 -0500

    Code part1

--- controllers/U3_real_exam_Quintal_Nora/U3_real_exam_Quintal_Nora.c
+++ controllers/U3_real_exam_Quintal_Nora/U3_real_exam_Quintal_Nora.c
@@ -14,7 +14,7 @@
 #include <webots/motor.h>
 #include <webots/distance_sensor.h>
 #include <webots/position_sensor.h>
-#include <webots/keyboard.h>
+
 
 #include <stdio.h>
 #include <math.h>
@@ -24,55 +24,68 @@
  */
 #define TIME_STEP 64
 #define PI 3.141592
-#define OBSTACLE_DISTANCE 55704.75
 
+#define OBSTACLE_DISTANCE 0.2
+#define RANGE 0.4 
+#define MAXRE 255  
+
+
+#define OBSTACLE_DISTANCE_2 0.72 
+#define RANGE2 2 
+#define MAXRE2 1023
+
+double encoder;
+double dis_1;
+double dis_2;  
 
 enum {
-  Go,
-  Turn,
-  TurnL,
-  TurnR,
-  left,
-  right,
+  GO,
+  TURN,
   FreeWay,
-  Obstacle
+  OBSTACLE,
+  LOOKING,
+  Enemy, 
+  ANYENEMY,
+  STOP
 };
 
-int state;
 double initial_angle_wheel1;
 
-int checkForObstacles(WbDeviceTag dis_S) {
-  double distance = wb_distance_sensor_get_value(dis_S);
+int checkForObstacles(WbDeviceTag dis_S1) {
+  double distance = wb_distance_sensor_get_value(dis_S1);
 
   if (distance > OBSTACLE_DISTANCE)
     return FreeWay;
   else
-    return Obstacle;
+    return OBSTACLE;
 }
+
+
+int checkForTheEnemy(WbDeviceTag distance_sensor) {
+  double distance2 = wb_distance_sensor_get_value(distance_sensor);
+  
+  dis2 = (distance2*RANGE2)/MAXRE2;
+  
+  if(dis2 <= 0.7) {
+      printf("The Enemy!: THA!\n");
+  }
+  
+  if (dis2 > OBSTACLE_DISTANCE_2)
+    return ANYENEMY;
+  else
+    return Enemy;
+}
+
+
 //////Functions for the robot
 /////avanzar
 void goRobot(WbDeviceTag *wheels, double velocity) {
+  wb_motor_set_position(wheels[0], INFINITY);
   wb_motor_set_velocity(wheels[0], 0);
+  wb_motor_set_position(wheels[1], INFINITY);
   wb_motor_set_velocity(wheels[1], velocity);
+  wb_motor_set_position(wheels[2], INFINITY);
   wb_motor_set_velocity(wheels[2], -velocity);
-}
-//retroceder
-void backRobot(WbDeviceTag *wheels) {
-  wb_motor_set_velocity(wheels[0], 0);
-  wb_motor_set_velocity(wheels[1], -6);
-  wb_motor_set_velocity(wheels[2], 6);
-}
-///izquierda
-void leftRobot(WbDeviceTag *wheels) {
-  wb_motor_set_velocity(wheels[0],-6);
-  wb_motor_set_velocity(wheels[1], 0);
-  wb_motor_set_velocity(wheels[2], 6);
- }
-////derecha
-void rightRobot(WbDeviceTag *wheels) {
-  wb_motor_set_velocity(wheels[0], 6);
-  wb_motor_set_velocity(wheels[1], 0);
-  wb_motor_set_velocity(wheels[2],-6);
 }
 ///detener
 void stopRobot(WbDeviceTag *wheels) {
@@ -80,20 +93,33 @@ void stopRobot(WbDeviceTag *wheels) {
   wb_motor_set_velocity(wheels[1], 0);
   wb_motor_set_velocity(wheels[2], 0);
 }
-///girar izquierda
-void turnLeft(WbDeviceTag *wheels) {
-  wb_motor_set_velocity(wheels[0], 6);
-  wb_motor_set_velocity(wheels[1], 6);
-  wb_motor_set_velocity(wheels[2], 6);
-}
+
 ///girar derecha
 void turnRight(WbDeviceTag *wheels) {
   wb_motor_set_velocity(wheels[0],-6);
   wb_motor_set_velocity(wheels[1],-6);
   wb_motor_set_velocity(wheels[2],-6);
 }
+
+void sensorRotate(WbDeviceTag *radar) {
+  wb_motor_set_position(radar[0], INFINITY);
+  wb_motor_set_velocity(radar[0], -3);
+}
+
+void stopSensor(WbDeviceTag *radar_gun) {
+  wb_motor_set_position(radar[0], INFINITY);
+  wb_motor_set_velocity(radar[0], 0);
+}
+
+void gunRotation(WbDeviceTag *gun, double pos_rad) {
+  wb_motor_set_position(gun[0], pos_rad);
+  
+}
+
+
 ///angulo
 double getAngleRobot(WbDeviceTag p_sen) {
+
   double angle_wheel1 = wb_position_sensor_get_value(p_sen);
   double angle;
 
@@ -102,6 +128,16 @@ double getAngleRobot(WbDeviceTag p_sen) {
   return angle;
 }
 
+
+double radarGetAngle(WbDeviceTag pos_rad){
+  double angle_radar = wb_position_sensor_get_value(radar_sensor);
+  double second_angle;
+  
+  second_angle= angle_radar;
+  
+  return second_angle;
+
+}
 /*
  * main
  */
@@ -109,21 +145,18 @@ int main(int argc, char **argv)
 {
   /* necessary to initialize webots stuff */
   wb_robot_init();
-  wb_keyboard_enable(TIME_STEP);
 
-  float velocity;
-  short int ds_state, ds_state1, robot_state = Go;
-  float angle;
-  float dis1, dis2;
    //inicializamos y guardamos en un array
    WbDeviceTag wheels[3];
      wheels[0] = wb_robot_get_device("wheel1");
      wheels[1] = wb_robot_get_device("wheel2");
      wheels[2] = wb_robot_get_device("wheel3");
      ///las hacemos rotar infinitamente
-   wb_motor_set_position (wheels[0], INFINITY);
-   wb_motor_set_position (wheels[1], INFINITY);
-   wb_motor_set_position (wheels[2], INFINITY);
+     //guardamos las variables del radar
+   WbDeviceTag radar[1]= wb_robot_get_device("RADAR");
+   WbDeviceTag Gun [1] = wb_robot_get_device("RT_GUN");
+
+
   ////obtenemos la info del sensor
    WbDeviceTag DSensor[2];
      DSensor[0] = wb_robot_get_device("DISTANCE_BODY");
@@ -188,7 +221,7 @@ int main(int argc, char **argv)
       }
      }
 
-         } else {
+        ) } else {
              stopRobot(wheels);
          }
           dis1 = wb_distance_sensor_get_value(DSensor[0]);
---
+++ controllers/U3_real_exam_Quintal_Nora/build/release/U3_real_exam_Quintal_Nora.d
@@ -0,0 +1,7 @@
+build/release/U3_real_exam_Quintal_Nora.o: U3_real_exam_Quintal_Nora.c \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h \
+ /usr/local/webots/include/controller/c/webots/motor.h \
+ /usr/local/webots/include/controller/c/webots/distance_sensor.h \
+ /usr/local/webots/include/controller/c/webots/position_sensor.h
